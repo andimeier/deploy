db1e301360db89537c07e1c237e0cd48228cad9e
Andi Meier <andi565656@server.fake>
2014-05-13 15:18:27 +0200

    made APP_SERVICE_NAME optional

--- stage-to-production/default/deploy_stage_to_production.sh
+++ stage-to-production/default/deploy_stage_to_production.sh
@@ -50,75 +50,12 @@ echo "Adapting symlink to current app version"
 rm $SYMLINK
 ln -s $ts $SYMLINK
 
-echo RESTARTING instance
-sudo service idot restart
-exit 0
-
-~
-~
-~
-~
-
-
-
-
-# Context: this git repo
-TMP_DIR=`mktemp -d`
-git archive HEAD | tar x -C $TMP_DIR
-
-# Context: $TMP_DIR
-cd $TMP_DIR
-
-cd app
-find -type f -name '.gitignore' -exec rm {} \;
-if ! npm install --production ; then
-	echo "Error at npm install. Aborting."
-	exit 2
-fi
-mv $TMP_DIR/node_modules/ $TMP_DIR/app/
-
-# add version file
-if [ "$VERSION_JSON" ] ; then
-	VERSION=`cat ../package.json | grep \"version\" | perl -p -e 's/^.*version.*\b(\d+\.\d+\.\d+)\b.*$/\1/g;'`
-	ts_json=`date '+%Y-%m-%d %H:%M:%S'`
-	echo '{' >${VERSION_JSON}
-	echo "  \"version\": \"$VERSION\"," >>${VERSION_JSON}
-	echo "  \"deployed\": \"$ts_json\"" >>${VERSION_JSON}
-	echo '}' >>${VERSION_JSON}
-	echo "------------------------------------"
-	echo "Extracted version: $a"
-	echo "Written version file ${VERSION_JSON}"
-	echo "------------------------------------"
+if [ ! -z "$APP_SERVICE_NAME" ] ; then
+	echo RESTARTING instance
+	sudo service $APP_SERVICE_NAME restart
 else
-	echo "No version info filename configured => no version info file written."
+	echo "Nothing to restart."
 fi
-
-# now copy the app "as is" into the app directory
-ts=`date '+%Y%m%d-%H%M%S'`
-TARGET_DIR=$DEPLOY_BASE_DIR/$ts
-mkdir $TARGET_DIR
-echo "Deploying to $TARGET_DIR ..."
-cp -r . $TARGET_DIR
-
-# Context: $DEPLOY_BASE_DIR
-cd $DEPLOY_BASE_DIR
-
-# add config file
-echo "Copying config files"
-
-# mkdir if not exists:
-mkdir -p $TARGET_DIR/$CONFIG_DEST
-cp -r $CONFIG_SRC/* $TARGET_DIR/$CONFIG_DEST
-
-echo "Adapting symlink to current app version"
-rm $SYMLINK
-ln -s $TARGET_DIR $SYMLINK
-
-echo Removing tmp dir [$TMP_DIR]
-rm -fr $TMP_DIR
-
-echo RESTARTING instance
-sudo service $APP_SERVICE_NAME restart
 
 echo
 echo '---------------------------------------------'
